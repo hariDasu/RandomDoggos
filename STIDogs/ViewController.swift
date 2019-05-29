d1d769e8f56a80dc0e9e5a37425aad4b09e13ddc
hariDasu <harirao3@gmail.com>
2019-05-28 21:16:54 -0400

    Initial Commit

---
+++ STIDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STIDogs
+//
+//  Created by Hari Rao on 5/28/19.
+//  Copyright © 2019 hariDasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
